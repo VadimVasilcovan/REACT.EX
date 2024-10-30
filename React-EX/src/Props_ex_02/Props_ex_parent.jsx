ce839ccb8ac9335e784a2813a05a4e0f88ac1ec0
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2024-10-30 23:20:12 +0200

    Props_ex_02
    I think i understood properly.

--- React-EX/src/Props_ex_02/Props_ex_parent.jsx
+++ React-EX/src/Props_ex_02/Props_ex_parent.jsx
@@ -3,16 +3,25 @@ import Joke from "./Jokes";
 function JokesApp () {
 
    
-    const dadJoke2 = "What do you call a French man wearing sandals? Philipe Fallop."
+    const dadJoke2 = {
+        joke: "What do you call a French man wearing sandals? Philipe Fallop.",
+        rating: 4
+    }
 
     return(
        <>
        
        <h1> Joke 1</h1>
        <Joke joke="bag pula in props" rating={3}/>
-       <Joke joke={dadJoke2} rating={4}/> 
+       <Joke joke={dadJoke2.joke} rating={dadJoke2.rating}/> 
+       {/*Bof variants are correct but we use '...' seldom when the object  have too mutch values*/}
+       {/*<Joke joke={dadJoke2.joke} rating={dadJoke2.rating}/>*/}
+       {/*<Joke {...dadJoke2}>*/}
+
        </>
+       
+
        
     )
 }
-export default JokesApp;
+export default JokesApp; 
