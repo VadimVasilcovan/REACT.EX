20baca4fbde34791bec952f286f92509582646d7
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2024-11-09 22:11:34 +0200

    Fetch()

--- React-EX/src/App.jsx
+++ React-EX/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+
 import ToDoList from "./Props_ex_03/parent-component";
 import FilterParent from "./Props_ex_04/filter-parent-ex4";
 import CarFunction from "./update-object-in-state/update-object";
@@ -12,6 +12,7 @@ import FetchDataex1 from "./useEfect-fetching-data/useEfect-fetching-data";
 import PokeApi from "./useEfect-fetching-data/pokeApi-fetch-ex";
 import GetData from "./Mastering-Fetch-API/basic-fetch-and-promises";
 import AsyncAwaytMethod from "./Mastering-Fetch-API/async-awayt";
+import UrlRequestHeaders from "./Mastering-Fetch-API/URL-Request-Headers";
 
 const App = () => {
     {/*const [submitPerson, setSubmitPerson] = useState([]);*/}
@@ -21,7 +22,7 @@ const App = () => {
             {/*<University setSubmitPerson={setSubmitPerson} />
             <ShowResult submitPerson={submitPerson} />
     */}
-       <AsyncAwaytMethod/>
+        <UrlRequestHeaders/>
         </>
     );
 };
--- React-EX/src/Mastering-Fetch-API/URL-Request-Headers.jsx
+++ React-EX/src/Mastering-Fetch-API/URL-Request-Headers.jsx
@@ -1,20 +1,32 @@
-import { useEffect } from "react"
-
+import { useEffect } from "react";
 
 const UrlRequestHeaders = () => {
+    const str = 'https://jsonplaceholder.typicode.com/users';
+    const url = new URL(str);
 
-    const str ='https://jsonplaceholder.typicode.com/users'
-    const url= new URL(str)
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const request = new Request(url, {
+                    headers: { 'x-steve': 'hello' },
+                    method: 'GET',
+                });
 
-    
-    useEffect(()=> {
+                const response = await fetch(request);
+                console.log(response.status);
+                
+                // Optional: If you need to log the response data
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
         
-        fetch(url)
-        console.log(url.host, url.origin, url.protocol), url.port
-    }, [])
-    return (<div>
+        fetchData();
+    }, []);
 
-    </div>)
-}
+    return <div></div>;
+};
 
-export default UrlRequestHeaders
+export default UrlRequestHeaders;
