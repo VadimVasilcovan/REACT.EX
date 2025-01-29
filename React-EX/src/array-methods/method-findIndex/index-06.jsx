4336ecc43f452282618f9c5b030d73d7bb3b352c
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-01-29 09:48:54 +0200

    29.01.2025

--- React-EX/src/array-methods/method-findIndex/index-06.jsx
+++ React-EX/src/array-methods/method-findIndex/index-06.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 export default function FindIndexEx06() {
   const [fetchApi, setFetchApi] = useState([]);
-  const [addToFavorite, setAdToFavorite] = useState([]);
+  const [addToFavorite, setAddToFavorite] = useState([]);
 
   async function fetchData() {
     try {
@@ -18,22 +18,32 @@ export default function FindIndexEx06() {
     fetchData();
   }, []);
 
+  const addToFavoriteFun = (value) => {
+    const checkTheIndex = addToFavorite.findIndex((i) => i.id === value.id);
+    if (checkTheIndex === -1) {
+      setAddToFavorite([...addToFavorite, value]);
+    }
+  };
+
   return (
     <div className="main-div-ex">
       <div className="pages-div">
         <p>List of person</p>
         {fetchApi.map(person => (
-            <ul key={person.id}>
-                <li >{person.name}</li>
-                <button>Add</button>
-            </ul>
-
+          <ul key={person.id}>
+            <li>{person.name}</li>
+            <button onClick={() => addToFavoriteFun(person)}>Add</button>
+          </ul>
         ))}
       </div>
       <div className="pages-div">
         <p>Favorite person</p>
+        {addToFavorite.map(person => (
+          <ul key={person.id}>
+            <li>{person.name}</li>
+          </ul>
+        ))}
       </div>
-      {console.log(fetchApi)}
     </div>
   );
 }
