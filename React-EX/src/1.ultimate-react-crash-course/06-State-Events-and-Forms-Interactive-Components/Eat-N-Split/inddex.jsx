bae42e3f7d349a9ad3f085f8d5ec2e2203c5c623
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-02-11 14:17:05 +0200

    11.02.2025

--- React-EX/src/1.ultimate-react-crash-course/06-State-Events-and-Forms-Interactive-Components/Eat-N-Split/inddex.jsx
+++ React-EX/src/1.ultimate-react-crash-course/06-State-Events-and-Forms-Interactive-Components/Eat-N-Split/inddex.jsx
@@ -25,7 +25,10 @@ export default function EatNSplit() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -55,17 +58,51 @@ function Friend({ friend }) {
       {friend.balance > 0 && (
         <p className="green">
           {friend.name} owes you {friend.balance}$
-        </p>)}
+        </p>
+      )}
 
-        { friend.balance === 0 &&(<p >
-          You and {friend.name} are even
-        </p>)}
-
-        <button className="button">Select</button>
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <Button>pulalau</Button>
     </li>
   );
 }
 
-function FormAddFriend(){
-    return <form className="form-add-friend"></form>
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend name</label>
+      <input type="text" />
+      <label>🌆Image URL</label>
+      <input type="text" />
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>🤑 Bill value</label>
+
+      <input type="text" />
+      <label>🙎🏻Your expense</label>
+
+      <input type="text" />
+      <label>🙆🏿 X's expense</label>
+
+      <input type="text" disabled />
+      <label>🫰🏿 Who is paying the bill</label>
+
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+      <Button>Split bill</Button>
+    </form>
+  );
 }
