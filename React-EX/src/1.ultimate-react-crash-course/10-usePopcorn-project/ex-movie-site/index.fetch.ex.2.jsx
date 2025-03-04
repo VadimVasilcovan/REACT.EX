86ffde285cf52dba236a6f17ae3ec8c10636f95d
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-04 09:21:54 +0200

    04.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-movie-site/index.fetch.ex.2.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-movie-site/index.fetch.ex.2.jsx
@@ -1,37 +1,80 @@
-import { useState } from "react"
+ import { useEffect, useState } from "react";
 
+export default function FetchEx04() {
+  const [query, setQuery] = useState("");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-export default function FetchEx04(){
-const [query, setQuery] = useState('inter')
-const [data, setData] = useState([])
-const [loading, setLoading] = useState(false)
-const [error, setError] = useState('')
+  useEffect(() => {
+    const controller =new AbortController
+    async function fetchMovies() {
+      try {
+        setLoading(true);
+        setError("");
+        const movieData = await fetch(
+          `http://www.omdbapi.com/?apikey=67c3761d&s=${query}`,
+          {signal: controller.signal}
+        );
+        if (!movieData.ok) throw new Error("fetch problem");
+        const movieResult = await movieData.json();
+        if (movieResult.Response === "False")
+          throw new Error("Movie not found");
 
-
-useState(()=>{
-    async function fetchMovies(){
-        try{
-            const movieData = await fetch(`http://www.omdbapi.com/?apikey=67c3761d&s=${query}`)
-            const movieResult = await movieData.json();
-            setData(movieResult?.Search || [])
-        }catch{
-
-        }finally{
-
-        }
+        setData(movieResult?.Search || []);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     }
-    fetchMovies()
-},[])
-    return(<>
-    <MovieList data={data}  />
-    </>)
+    fetchMovies();
+    return function(){
+        
+        controller.abort();
+    }
+  }, [query]);
+
+  return (
+    <>
+      {loading && <Loading />}
+      {!loading && !error && <MovieList data={data} />}
+      {error && <ErrorMessage error={error} />}
+
+      <SearchMovies query={query} setQuery={setQuery} />
+    </>
+  );
 }
 
-function MovieList({data}){
-return(<>
-{data.map((movie) => (
-    <h1 key={movie.imdbID}>{movie.Title}</h1>
-))}
-</>)
+function MovieList({ data }) {
+  return (
+    <>
+      {data.map((movie) => (
+        <h1 key={movie.imdbID}>{movie.Title}</h1>
+      ))}
+    </>
+  );
+}
+function ErrorMessage({ error }) {
+  return (
+    <>
+      <h1>{error}</h1>
+    </>
+  );
+}
 
-} 
+function SearchMovies({ query, setQuery }) {
+  return (
+    <>
+      <input value={query} onChange={(e) => setQuery(e.target.value)} />
+    </>
+  );
+}
+
+function Loading() {
+  return (
+    <>
+      <h1>🔃Loading ...</h1>
+    </>
+  );
+}
