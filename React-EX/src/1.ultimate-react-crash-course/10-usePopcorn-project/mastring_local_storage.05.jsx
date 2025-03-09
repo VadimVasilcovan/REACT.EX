2d7d0a7792c48fa3f195c3811ce5d2b000c32bc4
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-09 20:43:18 +0200

    09.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/mastring_local_storage.05.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/mastring_local_storage.05.jsx
@@ -1,35 +1,44 @@
 import { useEffect, useState } from "react";
 
+export default function LocalStorageEx05() {
+  const [data, setData] = useState("");
+  const [task, setTask] = useState(() => {
+    const setToLocalStorage = localStorage.getItem("LocalStorageToDo");
+    return setToLocalStorage ? JSON.parse(setToLocalStorage) : [];
+  });
 
-export default function LocalStorageEx05 (){
-    const [data, setData] = useState('')
-    const [task, setTask] = useState(() => {
-        const setToLocalStorage = localStorage.getItem('LocalStorageToDo')
-        return setToLocalStorage ? JSON.parse(setToLocalStorage) : []; 
-    })
-
-    const handleAddTask = () => {
-        if(data.length > 0){
-            setTask([...task, data])
-        setData('')
-        }
+  const handleAddTask = () => {
+    if (data.length > 0) {
+      setTask([...task, data]);
+      setData("");
     }
+  };
 
-    useEffect(()=>{
-        localStorage.setItem('LocalStorageToDo', JSON.stringify(task))
-    },[task])
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Enter") {
+        handleAddTask();
+      }
+    };
 
-  
-    return(<>
-    <input
-    value={data}
-    onChange = {(e) => setData(e.target.value)}
-    />
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document, removeEventListener("keydown", handleKeyDown);
+    };
+  }, [data]);
 
-    <button onClick={handleAddTask}>Add</button>
-        {task.map((toDos, index) => (
-            <p key={index}>{toDos}</p>
-        ))}
-    </>)
+  useEffect(() => {
+    localStorage.setItem("LocalStorageToDo", JSON.stringify(task));
+  }, [task]);
 
+  return (
+    <>
+      <input value={data} onChange={(e) => setData(e.target.value)} />
+
+      <button onClick={handleAddTask}>Add</button>
+      {task.map((toDos, index) => (
+        <p key={index}>{toDos}</p>
+      ))}
+    </>
+  );
 }
