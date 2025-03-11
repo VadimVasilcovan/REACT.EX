eedecdcfb3b9b28fea54b897faae4e74dcf331da
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-11 11:09:30 +0200

    11.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/mail-fomr.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/mail-fomr.jsx
@@ -1,26 +1,59 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function EnableDisableEx() {
   const [mail, setMail] = useState("");
-  const [addMail, setAddMail] = useState([])
+  const [addMail, setAddMail] = useState(()=>{
+    const locaLstorageFun = localStorage.getItem('addMail');
+    return locaLstorageFun ? JSON.parse(locaLstorageFun) : [];
+  });
   const [focus, setFocus] = useState(true);
+  const focusinput = useRef(null);
 
+  const handleAddMail = () => {
+    setAddMail([...addMail, mail]);
+    setMail("");
+  };
 
-    const handleAddMail =()=> {
-        setAddMail([...addMail, mail])
-    }
   useEffect(() => {
-    setFocus(!mail.includes("gmail"));
+    const EnterClick = (e) => {
+      if (e.key === "Enter") {
+        handleAddMail();
+      }
+    };
+    document.addEventListener("keydown", EnterClick);
+    return () => {
+      document.removeEventListener("keydown", EnterClick);
+    };
   }, [mail]);
+
+  useEffect(() => {
+    focusinput.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setFocus(!mail.includes("@"));
+  }, [mail]);
+
+  useEffect(()=>{
+    localStorage.setItem('addMail', JSON.stringify(addMail))
+  },[addMail])
 
   return (
     <>
-      <input value={mail} onChange={(e) => setMail(e.target.value)} />
-      <button onClick={handleAddMail} disabled={focus}>Submit</button>
-      <input disabled={true}/>
-      {addMail.map((mailsList, index) => (<div key={index}>
-        <p>{mailsList}</p>
-      </div>))}
+      <input
+        ref={focusinput}
+        value={mail}
+        onChange={(e) => setMail(e.target.value)}
+      />
+      <button onClick={handleAddMail} disabled={focus}>
+        Submit
+      </button>
+      <input disabled={true} />
+      {addMail.map((mailsList, index) => (
+        <div key={index}>
+          <p>{mailsList}</p>
+        </div>
+      ))}
     </>
   );
 }
