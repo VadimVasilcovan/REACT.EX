6642a5d6cc9f1c0765b4bbb18b64c02a59eba0b2
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-12 21:12:16 +0200

    12.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_1/index.01.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_1/index.01.jsx
@@ -1,33 +1,41 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function (){
 const [data, setData] = useState('')
-const [toDo, setToDo] = useState([])
+const [toDo, setToDo] = useState(()=>{
+    const GetFromLocalStorage = localStorage.getItem('fasfasf');
+    return GetFromLocalStorage ? JSON.parse(GetFromLocalStorage) : [];
+})
+const focus = useRef(null)
 
     function handleAddToDo(){
         if(data.includes('@')){
             setToDo([...toDo, data])
         }
-
-       
-
     } 
 
-
+    useEffect(()=>{
+        focus.current.focus();
+    },[])
     useEffect(()=>{
         const handleEnter = (e)=>{
             if(e.key === 'Enter'){
                 handleAddToDo
             }
         }
-
         document.addEventListener('keydown', handleEnter)
         return(
             document.removeEventListener('keydown', handleAddToDo)
         )
     },[data])
+
+    useEffect(()=>{
+        localStorage.setItem('fasfasf', JSON.stringify(toDo))
+    },[])
+
     return(<div>
         <input
+        ref={focus}
         onChange={(e) => setData(e.target.value)}
         value={data}
         />
