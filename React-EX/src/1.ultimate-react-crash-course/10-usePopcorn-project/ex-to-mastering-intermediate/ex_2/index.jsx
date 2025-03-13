384590ba019514679326056ce3ed8ff4c55a1241
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-13 22:31:23 +0200

    13.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_2/index.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_2/index.jsx
@@ -1,27 +1,52 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+export default function Exercise002() {
+  const [data, setData] = useState("");
+  const [toDo, setToDo] = useState(() => {
+    const handleLocalStorage = localStorage.getItem("hfss");
+    return handleLocalStorage ? JSON.parse(handleLocalStorage) : [];
+  });
+  const focusInput = useRef(null);
+  const handleAddToDo = () => {
+    if (data.length > 3) {
+      setToDo([...toDo, data]);
+      setData("");
+    }
+  };
 
-export default function Exercise002(){
-    const [data, setData] = useState('')
-    const [toDo, setToDo] = useState([])
+  useEffect(() => {
+    focusInput.current.focus();
+  }, []);
 
-    
-    const handleAddToDo = () =>{
-        if(data.length > 3){
-            setToDo([...toDo, data])
-        }
+  useEffect(() => {
+    function handleEnter(e) {
+      if (e.key === "Enter") {
+        handleAddToDo();
+      }
     }
 
+    document.addEventListener("keydown", handleEnter);
 
-    return(<>
-    <input
-    onChange={(e) => (setData(e.target.value))}
-    value={data}
-    />
-    <button onClick={handleAddToDo}>Add</button>
-    {toDo.map((toDos, index) =>(
+    return () => {
+      document.removeEventListener("keydown", handleEnter);
+    };
+  }, [data]);
+
+  useEffect(() => {
+    localStorage.getItem("hfss", JSON.stringify(toDo));
+  }, [toDo]);
+
+  return (
+    <>
+      <input
+        onChange={(e) => setData(e.target.value)}
+        value={data}
+        ref={focusInput}
+      />
+      <button onClick={handleAddToDo}>Add</button>
+      {toDo.map((toDos, index) => (
         <p key={index}>{toDos}</p>
-    ) )}
-    </>)
-
+      ))}
+    </>
+  );
 }
