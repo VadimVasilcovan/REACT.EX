1ac3bdb7d3f757f512b52efc2ed8541ce4d0d264
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-15 20:16:35 +0200

    15.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_3/index.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_3/index.jsx
@@ -1,22 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 
-export default function MasteringIntermediateEx3(){
-const [data, setData,] = useState('')
-const [task, setTask] = useState([])
+export default function MasteringIntermediateEx3() {
+  const [data, setData] = useState("");
+  const [task, setTask] = useState(() => {
+    const extractLocalStorageInfo = localStorage.getItem("addSomething");
+    return extractLocalStorageInfo ? JSON.parse(extractLocalStorageInfo) : [];
+  });
+  const HandleOnFocus = useRef(null);
+  function handleAddTask() {
+    setTask([...task, data]);
+    setData("");
+  }
 
+  useEffect(() => {
+    HandleOnFocus.current.focus();
+  }, []);
 
-function handleAddTask(){
-    setTask([...task, data])
-    setData('')
-}
+  useEffect(() => {
+    function handleEnterClick(e) {
+      if (e.key === "Enter") {
+        handleAddTask();
+      }
+    }
+    document.addEventListener("keydown", handleEnterClick);
 
-return(<div>
-    <input onChange={(e)=>setData(e.target.value)}
-    value={data}
-    />
-    <button onClick={handleAddTask}>Add A task</button>
-    {task.map((tasks, index) => (
+    return () => {
+      document.removeEventListener("keydown", handleEnterClick);
+    };
+  }, [data]);
+
+  useEffect(() => {
+    localStorage.setItem("addSomething", JSON.stringify(task));
+  }, [task]);
+  return (
+    <div>
+      <input
+        onChange={(e) => setData(e.target.value)}
+        value={data}
+        ref={HandleOnFocus}
+      />
+      <button onClick={handleAddTask}>Add A task</button>
+      {task.map((tasks, index) => (
         <p key={index}>{tasks}</p>
-    ))}
-</div>)
+      ))}
+    </div>
+  );
 }
