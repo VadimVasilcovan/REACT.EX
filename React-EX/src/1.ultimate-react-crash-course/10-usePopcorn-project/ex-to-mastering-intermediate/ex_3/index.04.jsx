5094e8a414b22bae45ddd31e92e3f3f4a8fba420
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-21 23:13:49 +0200

    21.03,2025

---
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_3/index.04.jsx
@@ -0,0 +1,49 @@
+import { useAddNewAgent } from "./useAddNewAgent";
+import { useHandleEnter } from "./handleEnter";
+
+export default function AgentData() {
+  const {
+    name,
+    age,
+    mail,
+    data,
+    succesMessage,
+    setName,
+    setAge,
+    setMail,
+    handleAddAgentData,
+  } = useAddNewAgent();
+
+  useHandleEnter(handleAddAgentData, name);
+
+  return (
+    <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          placeholder="Name"
+        />
+        <input
+          onChange={(e) => setAge(e.target.value)}
+          type="Number"
+          value={age}
+          placeholder="Age"
+        />
+        <input
+          onChange={(e) => setMail(e.target.value)}
+          value={mail}
+          placeholder="Email"
+        />
+        {succesMessage && <h2>Good, good!</h2>}
+        <button onClick={handleAddAgentData}>Add</button>
+      </form>
+
+      {data.map((agent, index) => (
+        <p key={index}>
+          {agent.name} - {agent.age} - {agent.mail}
+        </p>
+      ))}
+    </>
+  );
+}
