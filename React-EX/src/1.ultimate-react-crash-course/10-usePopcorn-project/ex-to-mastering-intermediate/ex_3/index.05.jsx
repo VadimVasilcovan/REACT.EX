c3e34a8e6d361fccff0704aed8ce63740779a4d1
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-23 22:41:30 +0200

    23.03.2025

---
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_3/index.05.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+
+export function useHandleEnter(interactionElemment, trigger) {
+  useEffect(() => {
+    function handleKeyPress(e) {
+      if (e.key === "Enter") {
+        interactionElemment();
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [interactionElemment, trigger]);
+}
+
+
+
