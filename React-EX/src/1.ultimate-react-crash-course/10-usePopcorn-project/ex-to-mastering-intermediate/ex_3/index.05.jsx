bf0ff70cd4b26c00dd1dfd921055ae66c975a3aa
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-03-24 18:57:28 +0200

    24.03.2025

--- React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_3/index.05.jsx
+++ React-EX/src/1.ultimate-react-crash-course/10-usePopcorn-project/ex-to-mastering-intermediate/ex_3/index.05.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Ex05() {
+export default function Ex05ex() {
   const [car, setCar] = useState("");
   const [mileage, setMileage] = useState("");
   const [productionYear, setProductionYear] = useState("");
@@ -9,23 +9,32 @@ export default function Ex05() {
   function handleAddCar(e) {
     e.preventDefault();
 
-    const NewCar = {
+    const newCar = {
       car,
       mileage,
       productionYear,
     };
 
     if (car && mileage && productionYear) {
-      setAddNewCar(...addNewCar, NewCar);
+      setAddNewCar([...addNewCar, newCar]);
+      setMileage('')
+      setProductionYear('')
+      setCar('')
     }
   }
 
   return (
+    <>
     <form onSubmit={handleAddCar}>
-      <input onChange={(e) => setCar(e.target.value)} />
-      <input onChange={(e) => setMileage(e.target.value)} />
-      <input onChange={(e) => setProductionYear(e.target.value)} />
-      <button></button>
+      <input value={car} onChange={(e) => setCar(e.target.value)} />
+      <input value={mileage} onChange={(e) => setMileage(e.target.value)} />
+      <input value={productionYear} onChange={(e) => setProductionYear(e.target.value)} />
+      <button>Add Car</button>
     </form>
+    {addNewCar.map((carsList, index) =>(
+        <h1 key={index}>{carsList.car} {carsList.productionYear}</h1>
+    ))}
+   
+    </>
   );
 }
