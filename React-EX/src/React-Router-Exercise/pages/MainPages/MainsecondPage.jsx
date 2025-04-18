cdd140d548567d3f387886a842f5116033da51b4
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-18 18:31:16 +0300

    18.04.2025

--- React-EX/src/React-Router-Exercise/pages/MainPages/MainsecondPage.jsx
+++ React-EX/src/React-Router-Exercise/pages/MainPages/MainsecondPage.jsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import ReacatRouteNavigation from '../../ReacatRouteNavigation'
-import { NavLink } from 'react-router-dom'
-import { usePostData } from '../../Context/contextData'
-
+import React from "react";
+import ReacatRouteNavigation from "../../ReacatRouteNavigation";
+import { NavLink } from "react-router-dom";
+import { usePostData } from "../../Context/contextData";
 
 export default function MainsecondPage() {
-  const {fetchedData} = usePostData()
-  console.log(fetchedData)
+  const { fetchedData } = usePostData();
+  console.log(fetchedData);
   return (
     <div>
-        <ReacatRouteNavigation/>
-        🏙️MainsecondPage🏙️
-
-        {fetchedData.map(datas=>(
-          
-          <NavLink  key={datas.id} to={`/second-main-page/${datas.id}`}>{datas.question}</NavLink>
-        ))}
-     
+      <ReacatRouteNavigation />
+      🏙️MainsecondPage🏙️
+      {fetchedData.map((datas) => (
+        <div key={datas.id}>
+          <NavLink to={`/second-main-page/${datas.id}`}>
+            {datas.question}
+          </NavLink>
         </div>
-
-  )
+      ))}
+    </div>
+  );
 }
- 
